990912944f7ac055f8d1d1903ab346cb07ede9eb
BrokenBrok <valerii.works@gmail.com>
2019-10-12 18:13:17 +0500

    ss

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_по_программированию__1__2_и_3_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Задание 1 (A. Поход в кино)
+
+            int N = int.Parse(Console.ReadLine()); // Кол-во ребят
+            int F = 0;
+
+            // Мне не удалось реализовать ввод чисел в одной строке
+            for (int i = 0; i < N; i++)
+            {
+                int y = int.Parse(Console.ReadLine());  // Кол-во денег у 1 ребенка
+                F += y;
+            }
+
+            int P = int.Parse(Console.ReadLine()); // Цена 1-го билета
+
+            int sum = N * P;
+            Console.WriteLine(F - sum);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Задание по программированию (1, 2 и 3)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Задание по программированию (1, 2 и 3)")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c0d05d70-4b51-463d-83f1-4828c57af59a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
